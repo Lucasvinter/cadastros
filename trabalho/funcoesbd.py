e9089b875a3e0d28e5158b288705a0f270bd88c1
unknown <900222@proway.treina>
2019-12-09 14:50:00 -0300

    backend cadastro

--- trabalho/funcoesbd.py
+++ trabalho/funcoesbd.py
@@ -1,15 +1,15 @@
 from conexao import conn, cursor
 
  #--- Método para cadastrar um funcionario
-def cadastrar_pessoa(self, nome, sobrenome, cpf, salario, cargo, carga_horaria, data_nascimento, sexo, lider):
+def cadastrar(self, nome, sobrenome, cpf, salario, cargo, carga_horaria, data_nascimento, sexo, lider):
     cursor.execute("INSERT INTO pessoas VALUES('{}', DEFAULT, '{}', '{}','{}', '{}', '{}', '{}', '{}', '{}')".format(salario, cargo, carga_horaria, nome, sobrenome, data_nascimento, cpf, sexo, lider))
     conn.commit()
 
 
 
-#--- Método para alterar uma pessoa --- Ainda não implementado 
+#--- Método para alterar um funcionario --- Ainda não implementado 
 def alterar(self, nome, sobrenome, cpf, salario, cargo, carga_horaria, data_nascimento, sexo, lider, id=int):
-    cursor.execute("UPDATE funcionarios SET nome = {}, sobrenome = {}, cpf = {} WHERE id = {}".format(nome, sobrenome, cpf, id))
+    cursor.execute("UPDATE funcionarios SET nome = {}, sobrenome = {}, cpf = {}, salario{}, cargo{}, carga_horaria{}, data_nascimento{}, sexo{}, lider{} WHERE id = {}".format(nome, sobrenome, cpf, salario, cargo, carga_horaria, data_nascimento, sexo, lider, id))
     conn.commit()
 
 #--- Método para listar todas as pessoas cadastradas
